473f79c0481a6cfed5a0ae62c4d669b1b7fc5b81
Ruandre Janse Van Rensburg <ruandrejvr@gmail.com>
2014-10-26 11:14:13 +0200

    Check for invalid ids and some refactoring

--- boxgallery.php
+++ boxgallery.php
@@ -4,7 +4,7 @@
   Plugin Name: BoxGallery
    Plugin URI: http://github.com/ruandre/boxgallery
   Description: A shortcode for displaying multiple lightbox galleries per post or page, each represented by a single image.
-      Version: 0.5
+      Version: 0.6
        Author: Ruandre Janse Van Rensburg
    Author URI: http://ruandre.com
       License: GNU General Public License v2 or later
@@ -24,21 +24,25 @@ if (!defined('WPINC')) die;
 
 
 
-function boxgallery_shortcode($atts, $content = null) {
+function boxgallery_shortcode($atts) {
 
   // Unique number for each shortcode:
   static $i = 0; $i++;
 
   // Make shortcode attributes available as variables:
-  extract(shortcode_atts(array('ids'   => false,
-                               'thumb' => false,
-                               'size'  => false), $atts));
+  extract(shortcode_atts(array(
+    'ids'   => false,
+    'thumb' => false,
+    'size'  => false
+  ), $atts));
 
   // Default image size:
-  if (!$size) $size = 'medium';
+  if (!$size)
+    $size = 'medium';
 
   // Split $ids string into an array:
-  if ($ids) $ids = explode(',', $ids);
+  if ($ids)
+    $ids = explode(',', $ids);
 
   $images = array();
 
@@ -51,45 +55,85 @@ function boxgallery_shortcode($atts, $content = null) {
   $boxgallery = false;
   $data       = " data-lightbox=\"set{$i}\"";
 
-  // Only one image? Let's make a single-image lightbox:
-  if (count($images) == 1) {
-    if ($thumb) $src = wp_get_attachment_image_src($thumb[0], $size); // Use thumbnail if set.
-    else        $src = wp_get_attachment_image_src($images[0], $size);
-               $href = wp_get_attachment_image_src($images[0], 'large');
-    return "<a href=\"{$href[0]}\"{$data}><img src=\"{$src[0]}\" alt=\"\"></a>";
-  }
+  if ($images && is_array($images) && (!empty($images))) {
+    if (count($images) > 1) {
 
-  // If we have attachment ids to work with:
-  if ($images && !empty($images) && (count($images) > 1)):
+      // Create thumbnail image from id:
+      if ($thumb) {
 
-    // Got thumbnail id? Let's use it:
-    if ($thumb) {
-      $src   = wp_get_attachment_image_src($thumb, $size);
-      $href  = wp_get_attachment_image_src($images[0], 'large');
-      $first = "<a href=\"{$href[0]}\"{$data}><img src=\"{$src[0]}\" alt=\"\"></a>";
-      array_shift($images); // Remove first image.
+        $src  = wp_get_attachment_image_src($thumb, $size);
+        $src  = ($src && !empty($src)) ? $src[0] : false;
+        $href = wp_get_attachment_image_src($images[0], 'large');
+        $href = ($href && !empty($href)) ? $href[0] : false;
+
+        $first = ($src && $href)
+          ? "<a href=\"{$href}\"{$data}><img src=\"{$src}\" alt=\"\"></a>"
+          : false;
+
+        array_shift($images); // Remove first image.
+
+      // No thumbnail id specified; use first image instead:
+      } else {
+
+        $first = array_shift($images); // Get first image and remove it.
+
+        $src  = wp_get_attachment_image_src($first, $size);
+        $src  = ($src && !empty($src)) ? $src[0] : false;
+        $href = wp_get_attachment_image_src($first, 'large');
+        $href = ($href && !empty($href)) ? $href[0] : false;
+
+        $first = ($src && $href)
+          ? "<a href=\"{$href}\"{$data}><img src=\"{$src}\" alt=\"\"></a>"
+          : false;
+
+      }
+
+      // Create a set using the remaining images:
+      foreach ($images as $image) {
+
+        $link = wp_get_attachment_image_src($image, 'large');
+        $link = ($link && !empty($link)) ? $link[0] : false;
+
+        if ($link)
+          $set .= "<a href=\"{$link}\"{$data}></a>";
+
+      }
+
+      // Wrap set in some handy markup:
+      if ($set)
+        $set = "<span class=\"boxgallery-set\">{$set}</span>";
+
+      // Wrap both first image and set in some handy markup:
+      if ($first && $set)
+        $boxgallery = "<span class=\"boxgallery\">{$first}{$set}</span>";
+
+
+    // Only one image? Let's make a single-image lightbox:
+    } else {
+
+      if ($thumb) {
+        $src = wp_get_attachment_image_src($thumb[0], $size);
+        $src = ($src && !empty($src)) ? $src[0] : false;
+      } else {
+        $src = wp_get_attachment_image_src($images[0], $size);
+        $src = ($src && !empty($src)) ? $src[0] : false;
+      }
+
+      $href = wp_get_attachment_image_src($images[0], 'large');
+      $href = ($href && !empty($href)) ? $href[0] : false;
+
+      if (current_user_can('edit_posts'))
+        $message = __("Invalid image ids; cannot display lightbox.", 'boxgallery');
+      else
+        $message = false;
+
+      if ($src && $href)
+        $boxgallery = "<a href=\"{$href}\"{$data}><img src=\"{$src}\" alt=\"\"></a>";
+      else
+        $boxgallery = $message;
     }
-    // No thumbnail id? Let's use the first image:
-    else {
-      $first = array_shift($images); // Get first image and remove it.
-      $src   = wp_get_attachment_image_src($first, $size);
-      $href  = wp_get_attachment_image_src($first, 'large');
-      $first = "<a href=\"{$href[0]}\"{$data}><img src=\"{$src[0]}\" alt=\"\"></a>";
-    }
 
-    // Create a set using all the remaining images:
-    foreach ($images as $image) {
-      $link = wp_get_attachment_image_src($image, 'large');
-      $set .= "<a href=\"{$link[0]}\"{$data}></a>";
-    }
-
-    // Wrap in some handy markup:
-    if ($set) $set = "<span class=\"boxgallery-set\">{$set}</span>";
-
-    if ($first && $set)
-      $boxgallery = "<span class=\"boxgallery\">{$first}{$set}</span>";
-
-  endif; // $images
+  } // if ($images)
 
   if ($boxgallery)
     return $boxgallery;
